055badcc4eeed4994c7d6b0f1a63276d82bcb94f
Abdelmoiz lebsir <133960178+LebsirAbdelmoiz@users.noreply.github.com>
2026-01-01 22:52:08 +0100

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,44 @@
-let duration = 1000;
-let parent = document.querySelector(".parent");
-let sons = Array.from(parent.children);
-let orderedsons = [...Array(sons.length).keys()];
+let duration = 1000,
+    parent = document.querySelector(".parent"),
+    sons = Array.from(parent.children),
+    orderedsons = [...Array(sons.length).keys()],
+    isstarted = false,
+    minutes = 0,
+    secondes = 0,
+    timerinterval,
+    finaltime,
+    matchcount = 0,
+    attempts=0,
+    startgame = document.querySelector(".startmenu span"),
+    playagain = document.querySelector('.endmenu span');
+    
+startgame.addEventListener('click', function(){
+    document.querySelector(".startmenu").classList.add('hidden');
+});
 shuffle(orderedsons);
 sons.forEach((son, index) => {
     son.style.order = orderedsons[index];
     son.addEventListener('click', function(){
+        if (!isstarted) {
+            isstarted=true;
+            starttimer();
+        }
         flip(son);
+        if (matchcount === 16) {
+            clearInterval(timerinterval);
+            finaltime = document.querySelector(".timer span").textContent;
+            setTimeout(() => {
+                document.querySelector('.endmenu').classList.add('nonhidden')
+                document.querySelector('.endmenu .text2 .timer').textContent = finaltime;
+                document.querySelector('.endmenu .text2 .attempts').textContent = attempts;
+            },1000);
+            playagain.addEventListener('click', function() {
+                location.reload();
+            });
+        }
     });
 });
+
 function shuffle(array) {
   let current = array.length,
       temp,
@@ -21,6 +51,17 @@ function shuffle(array) {
         array[random] = temp;
   }
   return array;
+}
+function starttimer() {
+    timerinterval = setInterval (() => {
+        if (secondes<(59)) {
+             secondes++;
+        } else {
+             secondes = 0;
+             minutes++;
+        }
+        document.querySelector('.timer span').textContent = String(minutes).padStart(2,'0')+':'+String(secondes).padStart(2,'0');
+    }, 1000);
 }
 function flip(selectedson) {  
     selectedson.classList.add('is-faced');
@@ -42,11 +83,15 @@ function checkmatched(first, seconde) {
         first.classList.add('is-matched');
         seconde.classList.remove('is-faced');
         seconde.classList.add('is-matched');
+        matchcount = matchcount + 2;
+        attempts +=1;
+        document.getElementById("attempts").textContent = "Attempts : "+String(attempts).padStart(2,"0");
     } else {
         setTimeout(() => {
             first.classList.remove('is-faced');
             seconde.classList.remove('is-faced');
         },duration)
-        
+        attempts +=1;
+        document.getElementById("attempts").textContent = "Attempts : "+String(attempts).padStart(2,"0");
     }
 }
